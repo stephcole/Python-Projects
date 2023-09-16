a55322a15604c05f735a7a58cccdb8dcc9025c27
stephcole <124626770+stephcole@users.noreply.github.com>
2023-09-16 00:37:29 -0400

    Add files via upload

--- web_page_generator.py
+++ web_page_generator.py
@@ -18,10 +18,13 @@ class ParentWindow(Frame):
         self.custom_btn.grid(row=3, column = 6, padx=(10, 10), pady=(10, 10))
 
         #creates entry for custom text
-        self.customHTML = Entry(text="Enter custom text or click Default HTML page button", width=75)
+        self.customHTML = Entry(self.master, text="Enter custom text or click Default HTML page button", width=75)
+        self.customHTML.insert(0, "Enter custom text or click Default HTML page button")
         #positions entry in GUI using tkinter grid() padx and pady are the same as
         #the button to ensure that will line up
         self.customHTML.grid(row=0, column=0, columnspan=7, padx=(20, 10), pady=(30, 0))
+
+
 
     def defaultHTML(self):
         htmlText = "Stay tuned for our amazing summer sale!"
@@ -32,7 +35,7 @@ class ParentWindow(Frame):
         webbrowser.open_new_tab("index.html")
 
     def customHTML(self):
-        htmlText = input("")
+        htmlText = self.customHTML.get()
         htmlFile = open("index.html", "w")
         htmlContent = "<html>\n<body>\n<h1>" + htmlText + "</h1>\n</body>\n</html>"
         htmlFile.write(htmlContent)
